9b1b947ffd0b2b715caf7328a096e790e6ef5cc7
sage henderson <2sagehenderson@gmail.com>
2021-11-18 01:51:58 -0700

    The base getAlbums endpoint has been generated, it will return all albums from https"//JSONplaceholder.typicode.com and post them to the person acessing the endpoint.

---
+++ CC1/Controllers/AlbumsController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace CC1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlbumsController : ControllerBase
+    {
+
+        // This default getAlbums endpoint will return all albums from https://jsonplaceholder.typicode.com/
+        [HttpGet]
+        public async Task<string> getAlbums()
+        {
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(@"https://jsonplaceholder.typicode.com");
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                return await client.GetStringAsync("/albums");
+
+            }
+
+        }
+
+        // This getAlbums will get any album that contains the search string in it's title
+
+
+
+        public class AlbumFormat
+        {
+            public string userId { get; set; }
+            public string id { get; set; }
+            public string title { get; set; }
+        
+        }
+
+
+
+    }
+}
