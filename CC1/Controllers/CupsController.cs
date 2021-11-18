f83b0a535cacba1a8156d55ddc272d5add6f75e7
sage henderson <2sagehenderson@gmail.com>
2021-11-18 00:46:35 -0700

    Added in Feature 1, the cup swapping code.  This includes both the cup swapping, and the BadRequest implementation for fail states.

--- CC1/Controllers/CupsController.cs
+++ CC1/Controllers/CupsController.cs
@@ -14,36 +14,73 @@ namespace CC1.Controllers
     {
 
         [HttpGet ("swap")]
-        public IEnumerable Swap([FromRoute] List<String> SwapCommands)
+        public object Swap([FromBody] IEnumerable<string> SwapCommands)
         {
             // The ball starts in the position of 'B'
             var ballLocation = "B";
+          
+            // Read the list of swap commands
 
-            // If there is no passed in list of swap commands return the starting location 'B'
-            if(SwapCommands == null)
-            {
-                return "The cups were not moved, the ball is in location" + ballLocation;
-            }
-            // If there is a list of swap commands, begin the process of swapping the balls location
-            else
-            {
-                // Read the list of swap commands
-
-                // Take the first one off the front (loop until all swaps have been read)
+            // Start with the first command (loop until all swaps have been read/swaps have been made)
             //{
-
+            foreach (string swap in SwapCommands)
+            {
                 // Check if it is a valid swap
+                if(swap == "AB" || swap == "BA")
+                {
+                    // If so, preform the swap
+                    if (ballLocation == "A") { ballLocation = "B"; }
+                    else if (ballLocation == "B") { ballLocation = "A"; }
+                }
+                else if(swap == "BC" || swap == "CB")
+                {
+                    // If so, preform the swap
+                    if (ballLocation == "B") { ballLocation = "C"; }
+                    else if (ballLocation == "C") { ballLocation = "B"; }
+                }
+                else if (swap == "AC" || swap == "CA")
+                {
+                    // If so, preform the swap
+                    if (ballLocation == "A") { ballLocation = "C"; }
+                    else if (ballLocation == "C") { ballLocation = "A"; }
+                }
+                else {
+                    // If it is not a valid swap return BadRequest message
+                    //{
 
-                // If it is not a valid swap return BadRequest message
+                    // If any swap contains more/less than two characters return a BadRequest with the appropriate message
+                    if (swap.Length != 2) { return BadRequest("The swap command is not in the appropriate format of two characters (Swap command length error)"); }
 
-                // Otherwise preform the swap
-            //}
+                    // If any swap has lowercase letters then return a BadRequest with the appropriate message
+                    // This needs to be placed before the Invalid char error, otherwise it won't be triggered (it is more specific than invalid char error)
+                    foreach (char swapChar in swap)
+                    {
+                        if (Char.IsLower(swapChar)) { return BadRequest("There is a lowercase character in a swap command (Lowercase error)"); }
+                    }
 
+                    // If any swap contains characters that are not A, B or C return a BadRequest with the appropriate message
+                    foreach (char swapChar in swap)
+                    {
+                        // Using the ASCII values of capital letters A, B and C (65, 66 and 67 respectively)
+                        // to determine if any of the characters in the swap request are invalid
+                        if ((int)swapChar < 65 || (int)swapChar > 67) { return BadRequest("There is an invalid character in the swap commands (Invalid char error)"); }
+                    }
+
+                    // If any swap is with itself (repeating cup name) return a BadRequest with the appropriate message
+                    if (swap == "AA" || swap == "BB" || swap == "CC") { return BadRequest("The swap can't occur with only one cup (Identical cup error)"); }
+
+
+                    // UNKNOWN ERROR message, in case there is a non-valid swap that the above don't catch (shouldn't be triggered unless the BadRequest checks miss something)
+                    BadRequest("UNKNOWN ERROR -- Invalid swap request");
+
+                    //} BadRequest checks/returns completed
+                }
+                //} Reading JSON string + Swapping cups completed
+                
+                // For checking what got passed in, uncomment for use
+                //return SwapCommands;
             }
-            // Return the final location of the ball
-            return ballLocation;
+            return "The final position of the ball is: " + ballLocation;
         }
-    
-
     }
 }
